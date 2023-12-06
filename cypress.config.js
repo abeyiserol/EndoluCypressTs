b9e61d64f70fe78f37deb31c0d7df0da27712188
abeyiserol <153075913+abeyiserol@users.noreply.github.com>
2023-12-06 18:14:57 +0300

    Update cypress.config.js
    added some commend to run jenkins

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'cypress';
-import allureWriter from '@shelex/cypress-allure-plugin/writer';
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
-export default defineConfig({
-	e2e: {
-		baseUrl: 'https://sandbox-api.endolu.com',
-		video: true,
-		setupNodeEvents(on, config) {
-			allureWriter(on, config);
-			return config;
-		},
-		env: {
-			allure: true,
-			allureAttachRequests: true,
-			allureAddVideoOnPass: true,
-		},
-	},
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://sandbox-api.endolu.com',
+    video: true,
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      allure: true,
+      allureAttachRequests: true,
+      allureAddVideoOnPass: true,
+      // Eklemek istediğiniz diğer ortam değişkenleri buraya ekleyebilirsiniz
+    },
+  },
 });
